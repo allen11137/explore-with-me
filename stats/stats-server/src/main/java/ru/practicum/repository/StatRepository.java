d077a79138c10851a0af6acc5d5aeade67e48d6e
allen11137 <122568848+allen11137@users.noreply.github.com>
2023-12-02 15:22:44 +0300

    Update 02.12(4)

--- stats/stats-server/src/main/java/ru/practicum/repository/StatRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.practicum.StatsDto;
 import ru.practicum.model.Stats;
 
 import java.time.LocalDateTime;
@@ -14,37 +13,37 @@ import java.util.List;
 public interface StatRepository extends JpaRepository<Stats, Integer> {
 
 
-    @Query(value = "select new ru.practicum.statsDto.ViewStats(eh.app, eh.uri, cast(count(eh.ip) " +
+    @Query(value = "select new ru.practicum.model.Stats(s.app, s.uri, cast(count(s.ip) " +
             "AS int) as hits) " +
-            "from EndpointHit as eh " +
-            "where eh.uri in ?3 " +
-            "and eh.timestamp >= ?1 " +
-            "and eh.timestamp <=?2 " +
-            "group by eh.app, eh.uri order by hits desc")
+            "from Stats as s " +
+            "where s.uri in ?3 " +
+            "and s.timestamp >= ?1 " +
+            "and s.timestamp <=?2 " +
+            "group by s.app, s.uri order by hits desc")
     List<Stats> findStats(LocalDateTime startTime, LocalDateTime endTime, Collection<String> uris);
 
-    @Query(value = "select new ru.practicum.statsDto.ViewStats(eh.app, eh.uri, cast(count(eh.ip) " +
+    @Query(value = "select new ru.practicum.model.Stats(s.app, s.uri, cast(count(s.ip) " +
             "AS int) as hits) " +
-            "from EndpointHit as eh " +
-            "where eh.timestamp >= ?1 " +
-            "and eh.timestamp <=?2 " +
-            "group by eh.app, eh.uri order by hits desc")
+            "from Stats as s " +
+            "where s.timestamp >= ?1 " +
+            "and s.timestamp <=?2 " +
+            "group by s.app, s.uri order by hits desc")
     List<Stats> findAllStats(LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query(value = "select new ru.practicum.statsDto.ViewStats(eh.app, eh.uri, cast(count(DISTINCT eh.ip) " +
+    @Query(value = "select new ru.practicum.model.Stats(s.app, s.uri, cast(count(DISTINCT s.ip) " +
             "AS int) as hits) " +
-            "from EndpointHit as eh " +
-            "where eh.uri in ?3 " +
-            "and eh.timestamp >= ?1 " +
-            "and eh.timestamp <=?2 " +
-            "group by eh.app, eh.uri order by hits desc")
+            "from Stats as s " +
+            "where s.uri in ?3 " +
+            "and s.timestamp >= ?1 " +
+            "and s.timestamp <=?2 " +
+            "group by s.app, s.uri order by hits desc")
     List<Stats> findUniqueStats(LocalDateTime startTime, LocalDateTime endTime, Collection<String> uris);
 
-    @Query(value = "select new ru.practicum.statsDto.ViewStats(eh.app, eh.uri,cast(count(DISTINCT eh.ip) " +
+    @Query(value = "select new ru.practicum.model.Stats(s.app, s.uri,cast(count(DISTINCT s.ip) " +
             "AS int) as hits) " +
-            "from EndpointHit as eh " +
-            "where eh.timestamp >= ?1 " +
-            "and eh.timestamp <=?2 " +
-            "group by eh.app, eh.uri order by hits desc")
+            "from Stats as s " +
+            "where s.timestamp >= ?1 " +
+            "and s.timestamp <=?2 " +
+            "group by s.app, s.uri order by hits desc")
     List<Stats> findAllUniqueStats(LocalDateTime startTime, LocalDateTime endTime);
 }
